a643583a1f8b55f1fe0aea36c045183829833297
Thiago Costa <thiagocosta.b2015@gmail.com>
2026-06-18 14:05:45 -0300

    feat: inicializa aplicação React com Vite, React Router, Axios e Context API

---
+++ taskflow-react/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import { TaskProvider } from "./context/TaskContext";
+
+import "./styles/style.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+
+  <TaskProvider>
+    <App />
+  </TaskProvider>
+
+);
